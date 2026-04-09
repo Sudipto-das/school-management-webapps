837dfe4fae9375a273ca94944731d3501f589b8f
sudipto das <sudiptodas877@gmail.com>
2026-04-09 08:49:52 +0530

    connect mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
+
+require('dotenv').config()
 const app = require('./src/app.js')
+const connectDB = require('./src/config/database.js')
+const dns = require("dns");
+
+// set custom DNS servers
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 
+connectDB()
 app.listen(7000,()=>{
     console.log("server is running on port 7000")
-});
+})
---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+async function connectDB() {
+    
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connected");
+    } catch (err) {
+        console.log("MongoDB Connection Error:", err);
+    }
+}
+
+module.exports = connectDB
